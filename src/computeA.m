dd9cc76efbaadc1ac3a687e8890e8f8fddc92dd1
Diego Martínez-Echevarría <diegome@mp.aau.dk>
2024-10-25 14:32:19 -0500

    Computation of A fully implemented

--- src/computeA.m
+++ src/computeA.m
@@ -1,5 +1,33 @@
-function A = computeA(N, L)
+function A = computeA(N, P, maxSize)
 %COMPUTEA Computes the set of all N-tuples of nonzero integers of sum L
-%   TODO implement
+
+    % First we solve the equivalent bars-and-stars (BS) problem
+    nBS = P + N - 1;
+    kBS = N - 1;
+    cardinality = nchoosek(nBS, kBS);
+    if nargin == 3 && cardinality > maxSize
+        msg = "The cardinality of A, %d, exceeds the maximum allowed size";
+        error(msg, cardinality);
+    end
+    barPosCombinations = nchoosek(1:nBS, kBS);
+
+    % We translate the bars and stars solution to our original formulation
+    A = zeros(cardinality, N);
+    for row = 1:cardinality
+        barPos = barPosCombinations(row, :);
+        A(row, :) = barPosToCoefficients(barPos, P);
+    end
 end
 
+function coefficients = barPosToCoefficients(barPositions, P)
+    N = length(barPositions) + 1;
+    barStarSum = N + P - 1;
+    coefficients = zeros(1, N);
+    
+    % First and last bin sizes need to be computed separately
+    coefficients(1) = barPositions(1) - 1;
+    coefficients(N) = barStarSum - barPositions(end);
+    for i = 2:length(barPositions)
+        coefficients(i) = barPositions(i) - barPositions(i-1) - 1;
+    end
+end
