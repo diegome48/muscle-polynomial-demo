a06176f7c82322cc5f2d57a6b102c9e2433bdbb5
Diego Martínez-Echevarría <diegome@mp.aau.dk>
2024-10-29 16:39:10 -0500

    Defined the tasks to be implemented

---
+++ main.m
@@ -0,0 +1,11 @@
+addpath src
+% Degree of the considered polynomials
+P = 3;
+% TODO create a Polynomial per representation
+% TODO create a Leg
+% TODO sample Leg N times, store muscle lengths and representations
+% TODO fit polynomials with the sampled data
+% TODO create trajectory with different values of kneeFlexionAngle
+% TODO for each value in the trajectory, compute the actual length of the
+% muscle and the predictions from the different Polynomials
+% TODO plot the different predictions together with the actual values
